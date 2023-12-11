a137b18a43b867b5efacce117a7d10155db9605b
Andriyrib <145540953+Andriyrib@users.noreply.github.com>
2023-12-11 19:34:30 +0200

    Add files via upload

--- task1.cpp
+++ task1.cpp
@@ -1,14 +1,1 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
 #include "task1.h"
-
-// Lab 1 Task 1
-
-int main(void) {
-    printf("%d ", 2 + 31);
-    printf("%d ", 45 * 54 - 11);
-    printf("%d ", 15 / 4);
-    printf("%f ", 15.0 / 4);
-    printf("%d ", 67 % 5);
-    printf("%f ", (2 * 45.1 + 3.2) / 2);
-}
